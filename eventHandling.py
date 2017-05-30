c4bdc322bcfcedda69ddbfe6107e9bd5160f93f4
taylorkm <taylor.kye@gmail.com>
2017-05-30 15:27:58 -0400

    version from class

--- eventHandling.py
+++ eventHandling.py
@@ -17,7 +17,7 @@ class Vectors():
 
   def onpick(self,event):    
     self.pressed = True
-
+    
   def onrelease(self, event):
     self.pressed = False
     self.fig.canvas.draw()
@@ -25,30 +25,35 @@ class Vectors():
   def onmotion(self, event):
     if self.pressed is False:
       return
+    # print(event.xdata,event.ydata)
     x = np.array( [[event.xdata],[event.ydata]] )
-    b = np.dot(A, x)
+    Ax = np.dot(A, x)
     
     # redo line coordinates
     self.iv.set_data([0,x[0]],[0,x[1]])
-    self.ov.set_data([0,b[0]],[0,b[1]])
+    self.ov.set_data([0,Ax[0]],[0,Ax[1]])
     self.fig.canvas.draw()
 
 
+
 # Define the 2-by-2 array
-A = np.array( [[1,1], [3,-2]] )
+A = np.array( [
+  [1,2],
+  [2,-4]
+  ] )
 
-# Define x
-x = np.array( [[1],[-1]] )
-
+# Define x and b
+x = np.array( [[1],[0]] )
+b = np.array( [[-2],[5]] )
 # Compute the matrix-vector product
-b = np.dot(A, x)
+Ax = np.dot(A, x)
 
 
 ## SETUP THE FIGURE, PLOT, AND INTERACTIVITY
 # For sizing
-extent = np.linalg.norm(A)
+extent = 2*np.linalg.norm(A)
 # Define plot parameters
-fig = plt.figure(figsize = (8,8))
+fig = plt.figure(figsize = (5,5))
 ax  = fig.add_subplot(111) # get Axes object
 ax.set_title('Matrix-Vector Products')
 
@@ -57,8 +62,9 @@ ax.plot([0,0],[-extent, extent], color='black')
 ax.plot([-extent, extent],[0,0], color='black')
 
 # plot the vectors
-inputVector, = ax.plot( [0,x[0]],[0,x[1]], linewidth = 2, label='Input',picker=5 ) 
-outputVector, = ax.plot( [0,b[0]],[0,b[1]], linewidth = 2, label='Output' ) 
+inputVector, = ax.plot( [0,x[0]],[0,x[1]], linewidth = 2, label='Input Vector = x',picker=5 ) 
+outputVector, = ax.plot( [0,Ax[0]],[0,Ax[1]], linewidth = 2, label='Output = Ax' ) 
+# bVector, = ax.plot( [0,b[0]],[0,b[1]], linewidth = 2, label='b' ) 
 
 # connect to controllers
 v = Vectors(inputVector,outputVector,fig)
@@ -71,4 +77,5 @@ ax.set_xlim(axisLimits[2:])
 ax.set_xlabel('x')
 ax.set_ylim(axisLimits[:2])
 ax.set_ylabel('y')
+plt.grid()
 plt.show()
